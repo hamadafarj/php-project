ed00516ba9d34065f5a8d5c497434a9415448c8a
hamadafarj <hamodmohamed1999@gmail.com>
2023-06-09 17:11:23 +0300

    complete assemint2"

--- Chapter 09/ch9.php
+++ Chapter 09/ch9.php
@@ -1,8 +1,19 @@
-<?php
-$email="anghamfakher@gmail.com";
-preg_match("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{3}$",$email);
-
-
-$phone_regex = "/^059{1}[98752]{1}[0-9]{6}$/";
-preg_match($phone_regex);
-//echo $pattern ;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $email="anghamfakher@gmail.com";
+    preg_match("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{3}$",$email);
+    
+    
+    $phone_regex = "/^059{1}[98752]{1}[0-9]{6}$/";
+    preg_match($phone_regex);
+    ?>
+</body>
+</html>
